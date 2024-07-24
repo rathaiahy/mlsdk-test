426ee013c31d7cea343ffcd6ac17b27e3fecff66
Sandro Pereira <38382721+54nd10@users.noreply.github.com>
2024-07-24 11:20:56 +0100

    Installed @magiclabs.ai/magicbook-client@0.7.11-canary

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var compression = require('compression');
+var express = require("express");
+var compression = require("compression");
 
 var app = express();
 app.use(compression());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', function (req, res) {
-  res.send('Hello, from page performance tweaking app!');
+app.get("/", function (req, res) {
+  res.send("Hello, from page performance tweaking app!");
 });
 app.listen(4000, function () {
-  console.log('App listening on port 4000!');
+  console.log("App listening on port 4000!");
 });
+
+console.log(__dirname);
+app.use(
+  "/scripts/magicbook-client",
+  express.static(__dirname + "/node_modules/@magiclabs.ai/magicbook-client")
+);
